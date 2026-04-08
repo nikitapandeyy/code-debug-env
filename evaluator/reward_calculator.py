02109ac36822be239822da8d926a2b0c98787a4c
nj7890 <43706707+nj7890@users.noreply.github.com>
2026-04-08 22:42:48 +0530

    Delete evaluator directory

--- evaluator/reward_calculator.py
+++
@@ -1,8 +0,0 @@
-def compute_reward(result):
-    if result["error"]:
-        return -0.2
-
-    if result["all_passed"]:
-        return 1.0
-
-    return (result["passed"] / result["total"]) * 0.5
